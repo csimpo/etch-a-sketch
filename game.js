19005535eb7e5bb4f058f4fa91e66eeccdf7035b
csimpo <csimszikrisztian88@gmail.com>
2024-05-22 14:55:11 +0200

    added basic elements

--- game.js
+++ game.js
@@ -1,13 +1,7 @@
-let container = document.querySelector("#container");
 
-for (let i=1; i<=16;i++){
-    let rows=document.createElement('div');
-    container.appendChild(rows);
+const container=document.querySelector(".container");
+for (let i=0; i<16*16; i++){
+    const divs=document.createElement("div");
+    container.appendChild(divs);
+    
 }
-
-for(let i=1; i<=16;i++){
-    let columns=document.createElement('div');
-    container.appendChild(columns);
-}
-
-
